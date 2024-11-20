9b16dcec82db0b6aae35bb40eef1230616bdedfe
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2024-11-20 22:07:36 +0200

    Created app routes and added navigation bar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,33 @@
 import { Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ExperienceComponent } from './experience/experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { CertificationsComponent } from './certifications/certifications.component';
+import { ContactMeComponent } from './contact-me/contact-me.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'home',
+    component: HomeComponent,
+  },
+  {
+    path: 'about-me',
+    component: ExperienceComponent,
+  },
+  {
+    path: 'projects',
+    component: ProjectsComponent,
+  },
+  {
+    path: 'certifications',
+    component: CertificationsComponent,
+  },
+  {
+    path: 'contact-me',
+    component: ContactMeComponent,
+  },
+  {
+    path: '**',
+    component: HomeComponent,
+  },
+];
