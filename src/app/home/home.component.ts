e0624d498c0766fb1e5160eb708868a7dfec53ba
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2024-12-07 00:48:17 +0200

    Testing add meta tags

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,12 +1,34 @@
 import { Component } from '@angular/core';
+import { Meta } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-home',
   standalone: true,
   imports: [],
   templateUrl: './home.component.html',
-  styleUrl: './home.component.scss'
+  styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-
+  constructor(private meta: Meta) {
+    meta.addTags([
+      {
+        name: 'description',
+        content:
+          "Panagiotis Skidas' portfolio website. Here you will be able to find all my projects and skills.",
+      },
+      {
+        name: 'keywords',
+        content: 'Angular, SEO, Home, Portfolio, Web, Panagiotis Sklidas',
+      },
+      { name: 'author', content: 'Panagiotis Sklidas' },
+      { property: 'og:title', content: 'Sklidas Panagiotis Portfolio' },
+      {
+        property: 'og:description',
+        content:
+          "Panagiotis Skidas' portfolio website. Here you will be able to find all my projects and skills.",
+      },
+      { property: 'og:type', content: 'website' },
+      { name: 'robots', content: 'index, follow' },
+    ]);
+  }
 }
