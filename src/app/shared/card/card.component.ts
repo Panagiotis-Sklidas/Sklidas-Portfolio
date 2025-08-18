7f86e0bbd4ed8eb9b0c04b5735cd575cb87ae2d4
Panagiotis Sklidas <panagiotis.skl@gmail.com>
2025-08-18 22:50:12 +0300

    Update Projects and Certification pages

--- src/app/shared/card/card.component.ts
+++ src/app/shared/card/card.component.ts
@@ -1,13 +1,26 @@
-import { Component, input } from '@angular/core';
+import { Component, Input, input, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
 
 @Component({
   selector: 'app-card',
   standalone: true,
-  imports: [],
+  imports: [MatCardModule],
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
 })
-export class CardComponent {
+export class CardComponent implements OnInit {
   imgPath = input();
   title = input();
+  descreption = input();
+  @Input() stack!: string[];
+  @Input() date!: string[];
+  link = input();
+  url = input<string>();
+
+  ngOnInit() {
+    // const startDate = this.date[0];
+    // const endDate = this.date[1];
+    // const site = this.url();
+    console.log('URL:', this.url());
+  }
 }
